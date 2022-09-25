3d25552d9183597666857c009bad961ce90712bf
Zakaria Yacoubi <49567393+YOPll@users.noreply.github.com>
2022-09-25 15:34:11 +0100

    unused

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
